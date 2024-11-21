4b7634f8369918cf2c7968e810668e0174a1f7bd
rowena <1374682909@qq.com>
2024-11-21 14:28:11 +0800

    othello

--- src/dashboard.tsx
+++ src/dashboard.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { createContext, useContext, useReducer, useState } from "react";
 import Button from "./Button";
-
+import { Status } from "./game";
+import Inboard from "./inboard";
 const DashBoard: React.FC = () => {
+    const [status, setStatus] = useState(new Status());
+    const statusContext = createContext(status);
     return (
         <div className="dashboard">
-            <div className="inboard"></div>
+            <statusContext.Provider value={status}>
+                <Inboard />
+            </statusContext.Provider>
             <Button left={140} onClick={() => alert('新游戏')}>新游戏</Button>
             <Button left={300} onClick={() => alert('Hint')}>提示</Button>
         </div >
---
+++ src/inboard.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+const Inboard: React.FC = () => {
+
+    return (
+        <div className="inboard">
+        </div>
+    )
+}
+export default Inboard
